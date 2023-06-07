8ca60c84cbbace91562d7d8d3a72ec743f58b6d9
BAE BYEONGIL <b000202@naver.com>
2023-06-07 09:21:50 +0800

    save part

--- src/detail.js
+++ src/detail.js
@@ -90,7 +90,12 @@ function clickhome () {
 }
 
 // 갑시다
-뭐를 포문으로 돌려달라고 말씀하신거죵?
+//뭐를 포문으로 돌려달라고 말씀하신거죵?
+//이걸 for문으로 돌려서 붙일 거예용
+// 가운데 조건은 우리가 작성한 만큼 붙일 거예요 일단은
+// 그렇게 하려면 작성자가 모여 있는 배열의 length를 가져와 주면 됩니다
+// 작성자가 모여 있는 배열은 writtersarray로 제가 만들었어요
+
 
 let p = `<p id="top">Review</p>
 <p class="content" >
@@ -100,6 +105,12 @@ ${localStorage.getItem(writtersarray[i] + sendid + "input")}
 <p class="content" >${writtersarray[i]} </p>
 <p id="id">Reivew date</p>
 <p class="content" >${localStorage.getItem(writtersarray[i] + sendid + "time")} </p>`
+
+for (let i=0; i < writterarray.length ; i++)
+
+// git add .
+// git checkout -b LEC
+// git
 
 
 
